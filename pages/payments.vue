5b70500256ddc9e30bbaaa92eeb74dbbf75a5ecb
Dave Calnan <d@ve.ie>
2018-11-24 04:22:52 +0000

    Debug: fuck this

--- pages/payments.vue
+++ pages/payments.vue
@@ -248,8 +248,7 @@
             <button
               type="submit"
               class="rounded-r-lg p-4 bg-indigo uppercase tracking-wide font-bold text-sm sm:text-base text-white border border-indigo sm:border-transparent focus:outline-none focus:shadow-outline"
-              v-text="Submit"
-            />
+            >Submit</button>
           </form>
           <small class="block pt-2 px-4 text-grey-darkest">
             We promise we will send exactly one email, written by a human. <em>No spam, no obligation.</em>
